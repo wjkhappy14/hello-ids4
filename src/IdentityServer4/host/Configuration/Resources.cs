f5e64ad41cc5e2ac4fa2407e7bffad34d20b8fc5
wjkhappy14@hotmail.com <wjkhappy14@hotmail.com>
2021-01-26 10:50:11 +0800

    init hello-ids4

---
+++ src/IdentityServer4/host/Configuration/Resources.cs
@@ -0,0 +1,81 @@
+// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityModel;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+using static IdentityServer4.IdentityServerConstants;
+
+namespace IdentityServerHost.Configuration
+{
+    public class Resources
+    {
+        // identity resources represent identity data about a user that can be requested via the scope parameter (OpenID Connect)
+        public static readonly IEnumerable<IdentityResource> IdentityResources =
+            new[]
+            {
+                // some standard scopes from the OIDC spec
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email(),
+                // custom identity resource with some consolidated claims
+                new IdentityResource("Exam", new[] { JwtClaimTypes.Name, JwtClaimTypes.Email, "location", JwtClaimTypes.Address })
+            };
+
+        // API scopes represent values that describe scope of access and can be requested by the scope parameter (OAuth)
+        public static readonly IEnumerable<ApiScope> ApiScopes =
+            new[]
+            {
+                // local API scope
+                new ApiScope(LocalApi.ScopeName),
+
+                // resource specific scopes
+                new ApiScope("address"),
+                new ApiScope("role"), 
+                
+                // a scope without resource association
+                // a scope shared by multiple resources
+                new ApiScope("phone"),
+
+                // a parameterized scope
+                new ApiScope("transaction", "Transaction")
+                {
+                    Description = "Some Transaction"
+                }
+            };
+
+        // API resources are more formal representation of a resource with processing rules and their scopes (if any)
+        public static readonly IEnumerable<ApiResource> ApiResources = 
+            new[]
+            {
+                new ApiResource("address", "Address")
+                {
+                    ApiSecrets = { new Secret("secret".Sha256()) },
+                    Scopes = { "resource1.scope1", "shared.scope" }
+                },
+                  new ApiResource("phone", "Phone")
+                {
+                    ApiSecrets = { new Secret("secret".Sha256()) },
+                    Scopes = { "resource1.scope1", "shared.scope" }
+                },
+
+                new ApiResource("role", "Role")
+                {
+                    ApiSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // additional claims to put into access token
+                    UserClaims =
+                    {
+                        JwtClaimTypes.Name,
+                        JwtClaimTypes.Email
+                    },
+
+                    Scopes = { "resource2.scope1", "shared.scope" }
+                }
+            };
+    }
+}
---
+++ src/IdentityServer4/host/Quickstart/TestUsers.cs
@@ -0,0 +1,66 @@
+// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityModel;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text.Json;
+using IdentityServer4;
+
+namespace IdentityServerHost.Quickstart.UI
+{
+    public class TestUsers
+    {
+        public static List<TestUser> Users
+        {
+            get
+            {
+                var address = new
+                {
+                    street_address = "One Hacker Way",
+                    locality = "Heidelberg",
+                    postal_code = 69118,
+                    country = "Germany"
+                };
+                
+                return new List<TestUser>
+                {
+                    new TestUser
+                    {
+                        SubjectId = "6292BF14-C0EC-F9A4-7FAD-39FA3489D6AC",
+                        Username = "alice",
+                        Password = "alice",
+                        Claims =
+                        {
+                            new Claim(JwtClaimTypes.Name, "Alice Smith"),
+                            new Claim(JwtClaimTypes.GivenName, "Alice"),
+                            new Claim(JwtClaimTypes.FamilyName, "Smith"),
+                            new Claim(JwtClaimTypes.Email, "AliceSmith@email.com"),
+                            new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                            new Claim(JwtClaimTypes.WebSite, "http://alice.com"),
+                            new Claim(JwtClaimTypes.Address, JsonSerializer.Serialize(address), IdentityServerConstants.ClaimValueTypes.Json)
+                        }
+                    },
+                    new TestUser
+                    {
+                        SubjectId = "0EF809A1-3C72-7CBE-79CB-39FA317823AC",
+                        Username = "bob",
+                        Password = "bob",
+                        Claims =
+                        {
+                            new Claim(JwtClaimTypes.Name, "Bob Smith"),
+                            new Claim(JwtClaimTypes.GivenName, "Bob"),
+                            new Claim(JwtClaimTypes.FamilyName, "Smith"),
+                            new Claim(JwtClaimTypes.Email, "BobSmith@email.com"),
+                            new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                            new Claim(JwtClaimTypes.WebSite, "http://bob.com"),
+                            new Claim(JwtClaimTypes.Address, JsonSerializer.Serialize(address), IdentityServerConstants.ClaimValueTypes.Json)
+                        }
+                    }
+                };
+            }
+        }
+    }
+}
