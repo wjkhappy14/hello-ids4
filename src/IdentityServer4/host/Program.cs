d588cec843b1672eb04e22f6cb62729a5d3f72fe
wjkhappy14@hotmail.com <wjkhappy14@hotmail.com>
2021-02-25 16:02:50 +0800

    https://jwt.io

--- src/IdentityServer4/host/Program.cs
+++ src/IdentityServer4/host/Program.cs
@@ -1,5 +1,4 @@
-// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+//https://www.cnblogs.com/huaweiyun/p/14411834.html
 
 
 using Microsoft.AspNetCore.Hosting;
